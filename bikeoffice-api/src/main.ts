fccac91b5da781c53f26e0e29afb0b8e71083d60
stonarini <stonarini@cifpfbmoll.eu>
2023-05-10 20:21:54 +0200

    dev: implement cookie and schema config in api

--- bikeoffice-api/src/main.ts
+++ bikeoffice-api/src/main.ts
@@ -1,25 +1,30 @@
 import express from 'express';
 import * as path from 'path';
-import { Test } from '@bikeoffice/types';
+import { Employee, Test, User } from '@bikeoffice/types';
+import sequelizeSchemaCrud from '@bikeoffice/sequelize-schema-connector';
+import sequelizeCrud from 'express-crud-router-sequelize-v6-connector'
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import crud from 'express-crud-router';
 
 const app = express();
 
 app.use(cors());
+app.use(cookieParser());
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-// log every request
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.path}`);
-    next();
+app.get('/', (req, res) => {
+    res.send({ message: 'Welcome to bikeoffice-api!' } as Test);
 });
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Welcome to bikeoffice-api!' } as Test);
-});
+app.get("/schema", (req, res) => res.cookie("schema", req.query.schema, { maxAge: 900000, httpOnly: false }) && res.send({ message: 'Schema set' }))
+
+app.use(crud('/users', sequelizeCrud(User)))
+
+app.use(crud('/employees', sequelizeSchemaCrud(Employee)))
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+    console.log(`Listening at http://localhost:${port}`);
 });
 server.on('error', console.error);
