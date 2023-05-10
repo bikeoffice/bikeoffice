be2eeea5b004f5719c249f77ffc751b822f1a6d5
stonarini <stonarini@cifpfbmoll.eu>
2023-05-10 20:15:17 +0200

    dev: add sequelize types and more dependencies

--- types/src/lib/types.ts
+++ types/src/lib/types.ts
@@ -5,3 +5,38 @@ export function types(): string {
 export type Test = {
     message: string;
 }
+
+import { Sequelize, DataTypes, Model } from 'sequelize';
+const sequelize = new Sequelize('postgres://bikeoffice:bikeoffice@localhost:5432/bikeoffice');
+
+export const User = sequelize.define('User', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  schema: {
+      type: DataTypes.STRING,
+      allowNull: false,
+  }
+});
+
+export const Employee = sequelize.define('Employee', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+//sequelize.createSchema('MegaRawBar', {});
+//sequelize.createSchema('VeloMallorca', {});
+//User.sync({force: true});
+//Employee.sync({schema: 'MegaRawBar', force: true});
+//Employee.sync({schema: 'VeloMallorca', force: true});
