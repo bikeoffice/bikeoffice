a9f40b33f65d6ea0b7ae018d015664a108b4540a
Joan Nicolau <joannicolau23@gmail.com>
2023-05-19 11:48:33 +0200

    feat/nav-animation
    commit e5e4dfb5a801bb7dbb7fafee9b098a67079bd04f
    Author: Joan Nicolau <joannicolau23@gmail.com>
    Date:   Fri May 19 11:48:08 2023 +0200
    
        add logic to handle current tab

--- bikeoffice-web/src/main/nav.tsx
+++ bikeoffice-web/src/main/nav.tsx
@@ -1,19 +1,39 @@
 import * as React from 'react';
 import { AppBar, Link, TitlePortal } from 'react-admin';
 import { Box, Tabs, Tab } from '@mui/material';
+import { useLocation } from 'react-router-dom';
 
 export const MainNav = () => {
-    const [value, setValue] = React.useState(2);
+    const linkStyle = { color: "white", fontSize: "16px", fontWeight: "800", opacity: 1 };
+    const location = useLocation();
+    const [value, setValue] = React.useState(0);
+
+    const routes = {
+        tpv: { value: 0, to: '/tpv', label: 'TPV' },
+        rent: { value: 1, to: '/rent', label: 'Rent' },
+        manage: { value: 2, to: '/manage', label: 'Manage' }
+    }
+
+    React.useEffect(() => {
+        const resource = routes[location.pathname.replace('/', '')];
+        if (resource) {
+            setValue(resource.value);
+        }        
+    }, [location]);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
 
     return (
-    <AppBar>
-        <TitlePortal />
-        <Tabs centered value={value} onChange={(_, newValue: number) => setValue(newValue)} >
-            <Link to="/tpv"><Tab sx={{color: "white", fontSize: "16px", fontWeight: "800", opacity: 1}} label="TPV"/></Link>
-            <Link to="/rent"><Tab sx={{color: "white", fontSize: "16px", fontWeight: "800", opacity: 1}}  label="Rent"/></Link>
-            <Link to="/manage"><Tab sx={{color: "white", fontSize: "16px", fontWeight: "800", opacity: 1}}  label="Manage"/></Link>
-        </Tabs>
-        <Box flex="1" />
-    </AppBar>
-)}
-
+        <AppBar>
+            <TitlePortal />
+            <Tabs centered value={value} onChange={handleChange}>
+                <Link to={routes.tpv.to}><Tab sx={linkStyle} label={routes.tpv.label} /></Link>
+                <Link to={routes.rent.to}><Tab sx={linkStyle} label={routes.rent.label} /></Link>
+                <Link to={routes.manage.to}><Tab sx={linkStyle} label={routes.manage.label} /></Link>
+            </Tabs>
+            <Box flex="1" />
+        </AppBar>
+    )
+}
