f7a5202e474226662091395d172d3526b1ad32d1
Joan Nicolau <joannicolau23@gmail.com>
2023-05-25 20:30:57 +0200

    event to select calendar renting

--- bikeoffice-web/src/modules/rent/calendar.tsx
+++ bikeoffice-web/src/modules/rent/calendar.tsx
@@ -6,9 +6,20 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 const localizer = dayjsLocalizer(dayjs);
 
 const events = [
-  // Add your events here
-];
-
+    {
+      id: 1,
+      title: 'Event 1',
+      start: new Date(2023, 4, 26, 10, 0), // May 26, 2023, 10:00 AM
+      end: new Date(2023, 4, 26, 12, 0), // May 26, 2023, 12:00 PM
+    },
+    {
+      id: 2,
+      title: 'Event 2',
+      start: new Date(2023, 4, 27, 14, 0), // May 27, 2023, 2:00 PM
+      end: new Date(2023, 4, 27, 16, 0), // May 27, 2023, 4:00 PM
+    },
+    // Add more events as needed
+  ];
 const eventStyleGetter = (event) => {
   const style = {
     backgroundColor: '#F0F2F5',
@@ -24,13 +35,19 @@ const eventStyleGetter = (event) => {
   };
 };
 
+const handleSelectEvent = (event) => {
+    // Handle event selection
+    console.log('Selected event:', event);
+  };
+
 export const MyCalendar = () => (
-  <div>
+  <div style={{marginTop: '20px'}}>
     <Calendar
       localizer={localizer}
       events={events}
       startAccessor="start"
       endAccessor="end"
+      onSelectEvent={handleSelectEvent}
       eventPropGetter={eventStyleGetter}
       style={{ height: 500 }}
     />
