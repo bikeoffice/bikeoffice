3c0be9e9ad09f91de293a27c9e36043a84cc317c
Joan Nicolau <joannicolau23@gmail.com>
2023-06-06 19:35:01 +0200

    Squashed commit of the following:
    commit d27c3e16d9348e59f42439234772f001ff0ff6b2
    Author: stonarini <stonarini@cifpfbmoll.eu>
    Date:   Tue Jun 6 18:29:49 2023 +0200
    
        hotfix/deploy-to-dev
    
    commit 92db9348df9462eba60828aea7046d8f35d64a10
    Author: stonarini <stonarini@cifpfbmoll.eu>
    Date:   Tue Jun 6 18:28:28 2023 +0200
    
        feat/deploy
    
    commit a407951f297e29195cf0aa42a035d14a4883ef32
    Author: stonarini <stonarini@cifpfbmoll.eu>
    Date:   Tue Jun 6 16:24:53 2023 +0200
    
        chore/cleanup

--- sql/users/create.sql
+++
@@ -1,9 +0,0 @@
--- Create the 'users' table in the 'public' schema
-CREATE TABLE public.users (
-    id INTEGER,
-    username VARCHAR(50),
-    password VARCHAR(50),
-    schema VARCHAR(50),
-    "createdAt" TIMESTAMP,
-    "updatedAt" TIMESTAMP
-);
