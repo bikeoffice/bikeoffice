97a7d94370a071e203037a6de68a7d4f971fc540
joan <joannicolau23@gmail.com>
2024-02-07 19:33:22 +0100

    feat/fix-initial-migration

--- types/src/lib/usersData.ts
+++ types/src/lib/usersData.ts
@@ -1,7 +1,11 @@
 export const usersData: string = `
 -- This file create the users table and insert the test users
 
+<<<<<<< Updated upstream
 CREATE TABLE IF NOT EXISTS public.users (
+=======
+CREATE TABLE IF NOT EXISTS users (
+>>>>>>> Stashed changes
   id INTEGER,
   username VARCHAR(255),
   password VARCHAR(255),
@@ -15,9 +19,9 @@ CREATE TABLE IF NOT EXISTS public.users (
   "createdAt" TIMESTAMP WITH TIME ZONE,
   "updatedAt" TIMESTAMP WITH TIME ZONE
 );
-`
+`;
 
 export const usersDataInsert: string = `
 INSERT INTO public.users (id, username, password, schema, logo, address, city, code, tel, website, "createdAt", "updatedAt") VALUES (1, 'testv', 'testv', 'VeloMallorca', '/api/assets/logovm.jpg', 'Carrer de l''Estacada 1', '07360 Palma', 'A40854069', '971 05 90 74', 'www.velomallorca.net', '2023-05-28 10:54:51.728387+00', '2023-05-28 10:54:51.728387+00');
 INSERT INTO public.users (id, username, password, schema, logo, address, city, code, tel, website, "createdAt", "updatedAt") VALUES (2, 'testm', 'testm', 'MegaRawBar', '/api/assets/logomrb.png', 'Carrer Joan Carles I 46', '07360 Lloseta', 'B67859017', '645 66 40 12', 'megarawbar.com', '2023-05-28 11:00:00.085073+00', '2023-05-28 11:00:00.085073+00');
-`
+`;
